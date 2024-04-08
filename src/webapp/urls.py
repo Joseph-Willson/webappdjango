d21f0864daeb8d6c793993b2d887a2d9a5aef311
jwillson <j.willson@outlook.fr>
2024-04-09 00:26:33 +0200

    debut dash

--- src/webapp/urls.py
+++ src/webapp/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import index, page2, aboutus, csv_table, variables_choisis, verification
+from .views import index, presentation, aboutus, csv_table, variables_choisis, verification, step1, step2, step3, step4, step5, step6, dashboard
 
 urlpatterns = [
     path('', index, name="webapp-index"),
-    path('page2/', page2, name="page2"),
+    path('presentation/', presentation, name="page2"),
     path('aboutus/', aboutus, name="aboutus"),
     path('csv_table/', csv_table, name='csv_table'),
     path('variables_choisis/', variables_choisis, name='variables_choisis'),
     path('verification/', verification, name='verification'),
-    
+    path('step1/', step1, name='step1'),
+    path('step2/', step2, name='step2'),
+    path('step3/', step3, name='step3'),
+    path('step4/', step4, name='step4'),
+    path('step5/', step5, name='step5'),
+    path('step6/', step6, name='step6'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
--- src/webapp/views.py
+++ src/webapp/views.py
@@ -4,19 +4,69 @@ import plotly.graph_objs as go
 import os
 from .forms import MyForm
 import json
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import io
 import base64
+
+import numpy as np
+from scipy.stats import chi2_contingency
+import seaborn as sns
+
+import statsmodels.api as sm
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 
 
 def index(request):
     return render(request, "webapp/index.html")
 
-def page2(request):
-    return render(request, "webapp/page2.html")
+######################################
+
+def presentation(request):
+    return render(request, "webapp/presentation.html")
+
+######################################
 
 def aboutus(request):
     return render(request, "webapp/aboutus.html")
+
+
+######################################
+
+def step1(request):
+    return render(request, "webapp/step1.html")
+
+
+######################################
+def step2(request):
+    return render(request, "webapp/step2.html")
+
+
+######################################
+def step3(request):
+    return render(request, "webapp/step3.html")
+
+
+######################################
+def step4(request):
+    return render(request, "webapp/step4.html")
+
+
+######################################
+def step5(request):
+    return render(request, "webapp/step5.html")
+
+
+######################################
+def step6(request):
+    return render(request, "webapp/step6.html")
+
+
+######################################
+
 
 def variables_choisis(request):
     if request.method == 'POST':
@@ -32,14 +82,13 @@ def variables_choisis(request):
             with open(file_path, "w") as file:
                 # Écrire les données dans le fichier
                 file.write(field_value)
-
     else:
         form = MyForm()
 
-    
-
     return render(request, "webapp/variables_choisis.html", {'form': form})
 
+
+#####################################
 
 def verification(request):
     graphs_base64 = []  # Pour stocker les images converties en base64
@@ -74,12 +123,99 @@ def verification(request):
         img_base64 = base64.b64encode(img_data.getvalue()).decode()
         graphs_base64.append(img_base64)
 
-        plt.close()  # Fermer le graphique pour libérer la mémoire
+        plt.close()
 
-    return render(request, "webapp/verification.html", {'graphs_base64': graphs_base64})
+    df_final=df_discretized[content+['TARGET']].copy()
+    
+    def cramers_v(df):
+
+        cols = df.columns
+        num_cols = len(cols)
+        result = np.zeros((num_cols, num_cols))
+        
+        for i in range(num_cols):
+            for j in range(i, num_cols):  # Modified to include diagonal elements
+                # Create contingency table for the pair of variables
+                contingency_table = pd.crosstab(df.iloc[:,i], df.iloc[:,j])
+                chi2, _, _, _ = chi2_contingency(contingency_table)
+                n = contingency_table.sum().sum()
+                phi2 = chi2 / n
+                r, c = contingency_table.shape
+                phi2corr = max(0, phi2 - ((r - 1) * (c - 1)) / (n - 1))
+                r_corr = r - ((r - 1) ** 2) / (n - 1)
+                c_corr = c - ((c - 1) ** 2) / (n - 1)
+                result[i, j] = np.sqrt(phi2corr / min((r_corr - 1), (c_corr - 1)))
+                result[j, i] = result[i, j]  # Matrix is symmetric
+        
+        result_df = pd.DataFrame(result, index=cols, columns=cols)
+
+        return result_df
+
+
+    result_matrix = cramers_v(df_final)
+
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(result_matrix, annot=True, cmap="coolwarm", fmt=".2f")
+    plt.title("Cramer's V Matrix")
+    
+    img_data_cramer = io.BytesIO()
+    plt.savefig(img_data_cramer, format='png')
+    img_data_cramer.seek(0)
+    img_base64_cramer = base64.b64encode(img_data_cramer.getvalue()).decode()
+
+    plt.close()
+
+    df_gs = df_final.copy()
+    df_gs = pd.get_dummies(df_gs, columns=content)
+
+    mean_values = {}
+
+    for col in content:
+        tx = df_final.groupby(col)['TARGET'].mean()
+        min_mean = tx.min()  
+        min_index = tx.idxmin()
+        mean_values[col] = (min_mean, min_index)
+    
+    for col in content:
+        df_gs = df_gs.drop([col + "_" + str(mean_values[col][1])], axis = 1)
+    
+    X = df_gs.drop('TARGET', axis=1)  
+    y = df_gs['TARGET']
+
+    # Diviser les données en ensemble d'entraînement et ensemble de test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+    # Initialiser le scaler et le modèle de régression logistique
+    scaler = StandardScaler()
+    # Standardiser les données d'entraînement et de test
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+
+    # Ajout d'une constante à la matrice des caractéristiques
+    X_train_sm = sm.add_constant(X_train_scaled)
+
+    # Création du modèle de régression logistique
+    logit_model = sm.Logit(y_train, X_train_sm)
+
+    # Ajustement du modèle
+    result = logit_model.fit()
+
+    # Prédictions sur l'ensemble de test
+    X_test_sm = sm.add_constant(X_test_scaled)
+    y_pred_proba_sm = result.predict(X_test_sm)
+
+    # Calcul de l'AUC
+    auc_sm = roc_auc_score(y_test, y_pred_proba_sm)
+    gini = 2 * auc_sm - 1
+
+    return render(request, "webapp/verification.html", {'graphs_base64': graphs_base64, 'vdecramer' : img_base64_cramer, 'auc' : round(auc_sm,2), 'gini' : round(gini,2)})
         
 
+######################################################
 
+def dashboard(request):
+    return render(request, "webapp/dashboard.html")
 
 
 
